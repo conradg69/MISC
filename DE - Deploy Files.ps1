c38bf69cf842ab38347d7305868bf3e6d0beadca
conradg69 <36333522+conradg69@users.noreply.github.com>
2019-08-29 16:20:48 +0100

    Minor Updates

--- DE - Deploy Files.ps1
+++ DE - Deploy Files.ps1
@@ -3,8 +3,8 @@ $SiteBLocations = @(
     "\\Vruk-b-macSQL03\c$\Services\DiscountEngineService","\\Vruk-b-macSQL04\c$\Services\DiscountEngineService",
     "\\Vruk-b-macSQL02\c$\Services\DiscountEngineService","\\Vruk-b-macSQL01\c$\Services\DiscountEngineService",
     "\\Vruk-b-ILTSQL01\c$\Services\DiscountEngineService","\\Vruk-b-ILTSQL02\c$\Services\DiscountEngineService",
-    "\\Vruk-b-ILTSQL03\c$\Services\DiscountEngineService","\\Vruk-b-ILTSQL04\c$\Services\DiscountEngineService"
-    "\\Vruk-b-ILTSQL05\c$\Services\DiscountEngineService", "\\Vruk-b-ILTSQL06\c$\Services\DiscountEngineService",
+    "\\Vruk-b-ILTSQL03\c$\Services\DiscountEngineService","\\Vruk-b-ILTSQL04\c$\Services\DiscountEngineService",
+    "\\Vruk-b-ILTSQL05\c$\Services\DiscountEngineService","\\Vruk-b-ILTSQL06\c$\Services\DiscountEngineService",
     "\\Vruk-b-ILTSQL07\c$\Services\DiscountEngineService"
                     )
                 
